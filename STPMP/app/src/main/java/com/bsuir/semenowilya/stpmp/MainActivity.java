2193252486619fb2d9fc726b1aeb3e5a813ffcfd
Ilya Semenow <ilya.semenow@intellectsoft.net>
2016-12-10 02:20:27 +0300

    Lab 1 finished.

--- STPMP/app/src/main/java/com/bsuir/semenowilya/stpmp/MainActivity.java
+++ STPMP/app/src/main/java/com/bsuir/semenowilya/stpmp/MainActivity.java
@@ -1,7 +1,12 @@
 package com.bsuir.semenowilya.stpmp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openLabOne(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        startActivity(intent);
+    }
+
+    public void openLabTwo(View view) {
+
+    }
+
+
+    public void openLabThree(View view) {
+
+    }
+
+    public void openLabKR(View view) {
+
     }
 }
